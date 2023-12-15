1c6e968c9d559eeabf36262eb1dfcff2766e0f6d
miladmahmoudzadeh <miiladiighj@gmail.com>
2023-12-15 06:40:41 -0800

    upload coin project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRoutes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import routes from "./router";
+import { useEffect } from "react";
+
+import "./responsive.css";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    document.body.style.backgroundColor = "black";
+  });
+  const router = useRoutes(routes);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Header />
+        {router}
+      </div>
     </div>
   );
 }
